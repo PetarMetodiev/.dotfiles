b05bc042296deebd8465e304fa2b848edafde4de
Petar Metodiev <petar.metodiev@gmail.com>
2021-09-23 10:07:31 +0300

    nvim: improve treesitter config
    Add needed languages and enable incremental selection.

--- vim/lua-nvim/nvim/lua/configs/treesitter.lua
+++ vim/lua-nvim/nvim/lua/configs/treesitter.lua
@@ -1,6 +1,29 @@
 require('utils')
 
 require('nvim-treesitter.configs').setup({
+  ensure_installed = {
+    'bash',
+    'css',
+    'scss',
+    'html',
+    'javascript',
+    'jsdoc',
+    'json',
+    'json5',
+    'lua',
+    'tsx',
+    'typescript',
+    'vim'
+  },
+  incremental_selection = {
+  enable = true,
+  keymaps = {
+    init_selection = 'gnn',
+    node_incremental = 'grn',
+    scope_incremental = 'grc',
+    node_decremental = 'grm',
+    },
+  },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = true,
