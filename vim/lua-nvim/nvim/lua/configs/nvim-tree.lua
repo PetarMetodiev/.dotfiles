d130450bd7235d49ba06d279b33ca5fdb319155f
Petar Metodiev <petar.metodiev@gmail.com>
2021-09-26 10:54:14 +0300

    nvim: update nvim-tree config
    It seems a bit broken so trying out new config options - neither the
    docs are written in an understandable language, nor the options do
    anything meaningful.

--- vim/lua-nvim/nvim/lua/configs/nvim-tree.lua
+++ vim/lua-nvim/nvim/lua/configs/nvim-tree.lua
@@ -95,6 +95,9 @@ require'nvim-tree'.setup ({
   -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
   update_focused_file = {
     -- enables the feature
-    enable      = true
+    enable      = true,
+    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+    -- only relevant when `update_focused_file.enable` is true
+    update_cwd  = true,
   },
 })
