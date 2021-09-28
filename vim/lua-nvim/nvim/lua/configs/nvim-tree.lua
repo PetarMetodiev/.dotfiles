4595533a2f325b47d36b7f2f0781a09445ab89fe
Petar Metodiev <petar.metodiev@gmail.com>
2021-09-28 18:35:30 +0300

    nvim: update nvim-tree config
    Again!

--- vim/lua-nvim/nvim/lua/configs/nvim-tree.lua
+++ vim/lua-nvim/nvim/lua/configs/nvim-tree.lua
@@ -2,7 +2,7 @@ local utils = require('utils')
 local tree_width = 30
 
 --30 by default, can be width_in_columns or 'width_in_percent%'
-g.nvim_tree_width = tree_width
+-- g.nvim_tree_width = tree_width
 
 g.nvim_tree_ignore = { '.git', '.cache' }
 
@@ -53,7 +53,7 @@ end
 
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 
-vim.g.nvim_tree_bindings = {
+local list = {
   { key = 'C',                            cb = tree_cb('cd') },
   { key = '<C-v>',                        cb = tree_cb('vsplit') },
   { key = '<C-s>',                        cb = tree_cb('split') },
@@ -82,22 +82,35 @@ vim.g.nvim_tree_bindings = {
 }
 
 require'nvim-tree'.setup ({
-  -- open the tree when running this setup function
-  open_on_setup       = true,
-  -- closes neovim automatically when the tree is the last **WINDOW** in the view
-  auto_close          = true,
-  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-  open_on_tab         = true,
-  -- hijack the cursor in the tree to put it at the start of the filename
-  hijack_cursor       = false,
-  -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually) 
-  update_cwd          = true,
-  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-  update_focused_file = {
-    -- enables the feature
-    enable      = true,
-    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-    -- only relevant when `update_focused_file.enable` is true
-    update_cwd  = true,
-  },
+    -- open the tree when running this setup function
+    open_on_setup       = true,
+    -- closes neovim automatically when the tree is the last **WINDOW** in the view
+    auto_close          = true,
+    -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+    open_on_tab         = true,
+    -- hijack the cursor in the tree to put it at the start of the filename
+    hijack_cursor       = false,
+    -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually) 
+    update_cwd          = true,
+    -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+    update_focused_file = {
+      -- enables the feature
+      enable      = false,
+      -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+      -- only relevant when `update_focused_file.enable` is true
+      update_cwd  = false,
+    },
+    view = {
+      -- width of the window, can be either a number (columns) or a string in `%`
+      width = tree_width,
+      -- if true the tree will resize itself after opening a file
+      auto_resize = true,
+      mappings = {
+        -- custom only false will merge the list with the default mappings
+        -- if true, it will only use your list to set the mappings
+        custom_only = false,
+        -- list of mappings to set on the tree manually
+        list = list
+      }
+    }
 })
