07e0617614e9259b0c6baeb6c2712e180ae1eba9
Petar Metodiev <petar.metodiev@gmail.com>
2021-10-23 20:51:49 +0300

    nvim: update colorscheme config
    Another broken thing that needed updating because of reasons.

--- vim/lua-nvim/nvim/lua/configs/colorscheme.lua
+++ vim/lua-nvim/nvim/lua/configs/colorscheme.lua
@@ -3,39 +3,48 @@ require('utils')
 local M = {}
 
 M.setup_onedark = function()
-  local onedark = require('onedark')
+  local onedark = require('onedarkpro')
   onedark.setup({
-      colors = {
-        yellow = '#F9FF8D',
-        red = '#Cb4F10',
-        green = '#52AD70',
-        purple = '#B164C8',
-        blue = '#83C0F2',
-        cyan = '#39939F',
-        white = '#ABB2BF',
-        black = '#1e1e1e',
-        gray = '#4C4F55',
-        highlight = '#E2BE7D',
-      };
-      styles = {
-        comments = 'italic',
-        functions = 'NONE',
-        keywords = 'bold,italic',
-        strings = 'NONE',
-        variables = 'NONE'
-      };
-      hlgroups = {
-        CursorLine = { bg = '#2C323C' },
-        MsgArea = { fg = 'cleared' },
-        Pmenu = { bg='#3E4452' },
-        PmenuSel = { fg='#282C34', bg='#61AFEF' },
-        PmenuSbar = { bg='#3B4048' },
-        PmenuThumb = { bg='#ABB2Bf' },
-        LineNr = { fg='LightGrey', bg='#808080' },
-        MatchParen = { bg='#ee88ee', fg='#23272e', style='bold' },
-        NvimTreeOpenedFile = { bg = '#4C4F55', style='bold' }
-      }
-    })
+    theme = "onedark",
+    colors = {
+      yellow = '#F9FF8D',
+      red = '#Cb4F10',
+      green = '#52AD70',
+      purple = '#B164C8',
+      blue = '#83C0F2',
+      cyan = '#39939F',
+      white = '#ABB2BF',
+      black = '#1e1e1e',
+      gray = '#4C4F55',
+      highlight = '#E2BE7D',
+    };
+    styles = {
+      comments = 'italic',
+      functions = 'NONE',
+      keywords = 'bold,italic',
+      strings = 'NONE',
+      variables = 'NONE'
+    };
+    hlgroups = {
+      CursorLine = { bg = '#2C323C' },
+      MsgArea = { fg = 'cleared' },
+      Pmenu = { bg='#3E4452' },
+      PmenuSel = { fg='#282C34', bg='#61AFEF' },
+      PmenuSbar = { bg='#3B4048' },
+      PmenuThumb = { bg='#ABB2Bf' },
+      LineNr = { fg='LightGrey', bg='#808080' },
+      MatchParen = { bg='#ee88ee', fg='#23272e', style='bold' },
+      NvimTreeOpenedFile = { bg = '#4C4F55', style='bold' }
+    },
+    options = {
+      bold = true, -- Use the themes opinionated bold styles?
+      italic = true, -- Use the themes opinionated italic styles?
+      underline = true, -- Use the themes opinionated underline styles?
+      undercurl = true, -- Use the themes opinionated undercurl styles?
+      transparent = false, -- Use a transparent background?
+      highlight_cursorline = true, -- Use cursorline highlighting?
+    }
+  })
   onedark.load()
 end
 
