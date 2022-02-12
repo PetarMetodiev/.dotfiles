1ecaed3341dcd45343100c0cac089607385e0744
Petar Metodiev <petar.metodiev@gmail.com>
2022-02-12 20:11:38 +0200

    nvim: make loading onderkpro safer

--- vim/lua-nvim/nvim/lua/user/plugins.lua
+++ vim/lua-nvim/nvim/lua/user/plugins.lua
@@ -26,7 +26,7 @@ vim.cmd [[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  print('There was a problem loading packer.')
+  vim.notify('There was a problem loading packer.')
   return
 end
 
