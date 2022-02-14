8700bf7905d8f780845ac84c4976788c325f01a7
Petar Metodiev <petar.metodiev@gmail.com>
2022-02-14 19:11:43 +0200

    nvim: move plugin reload autocmd to autocmd file

--- vim/lua-nvim/nvim/lua/user/plugins.lua
+++ vim/lua-nvim/nvim/lua/user/plugins.lua
@@ -16,13 +16,6 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 -- Autocommand that reloads neovim whenever this file is saved
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
-
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
