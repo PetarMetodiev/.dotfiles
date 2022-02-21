b6930fea8b2c4e0cfeb6dc0bd239c5fea0ee17b4
Petar Metodiev <petar.metodiev@gmail.com>
2022-02-21 20:42:25 +0200

    nvim: add indentation option for treesitter

--- vim/lua-nvim/nvim/lua/user/configs/treesitter.lua
+++ vim/lua-nvim/nvim/lua/user/configs/treesitter.lua
@@ -21,17 +21,19 @@ configs.setup({
     'typescript',
     'vim'
   },
+  sync_install = false,
   incremental_selection = {
-  enable = true,
-  keymaps = {
-    init_selection = 'gnn',
-    node_incremental = 'grn',
-    scope_incremental = 'grc',
-    node_decremental = 'grm',
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm',
     },
   },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = true,
-  }
+  },
+  indent = { enable = true, disable = { "yaml" } }
 })
