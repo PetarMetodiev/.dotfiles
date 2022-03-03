2b278934ed3ec5c28d8a47f59140cf09b56fb1c6
Petar Metodiev <petar.metodiev@gmail.com>
2022-03-03 15:59:29 +0200

    nvim: always open nvim-tree on current file

--- vim/lua-nvim/nvim/lua/user/configs/nvim-tree.lua
+++ vim/lua-nvim/nvim/lua/user/configs/nvim-tree.lua
@@ -12,15 +12,13 @@ g.nvim_tree_indent_markers = 1
 g.nvim_tree_git_hl = 1
 
 -- will enable folder and file icon highlight for opened files/directories.
-g.nvim_tree_highlight_opened_files = 3
-
--- g.nvim_tree_root_folder_modifier = ':~' --This is the default. See :help filename-modifiers for more options
+g.nvim_tree_highlight_opened_files = 2
 
 -- List of filenames that gets highlighted with NvimTreeSpecialFile
 -- There is some kind of bug - removing this line, removes README.md icon
 g.nvim_tree_special_files = { 'README.md'; 'Makefile'; 'MAKEFILE'; }
 
-nnoremap('<C-b>', '<cmd>NvimTreeToggle<CR>')
+nnoremap('<C-b>', '<cmd>NvimTreeFindFileToggle<CR>')
 
 local is_expanded = false
 function toggle_expanded()
@@ -112,3 +110,84 @@ require'nvim-tree'.setup ({
       }
     }
 })
+--[[
+{
+      disable_netrw        = false,
+      hijack_netrw         = true,
+      open_on_setup        = false,
+      ignore_buffer_on_setup = false,
+      ignore_ft_on_setup   = {},
+      hijack_directories   = {
+        enable = true,
+        auto_open = true,
+      },
+      auto_close           = false,
+      auto_reload_on_write = true,
+      open_on_tab          = false,
+      hijack_cursor        = false,
+      update_cwd           = false,
+      hijack_unnamed_buffer_when_opening = false,
+      diagnostics          = {
+        enable = false,
+        show_on_dirs = false,
+        icons = {
+          hint = "",
+          info = "",
+          warning = "",
+          error = "",
+        }
+      },
+      update_focused_file = {
+        enable      = false,
+        update_cwd  = false,
+        ignore_list = {}
+      },
+      system_open = {
+        cmd  = nil,
+        args = {}
+      },
+      git = {
+        enable = true,
+        ignore = true,
+      },
+      view = {
+        width = 30,
+        height = 30,
+        side = 'left',
+        preserve_window_proportions = false,
+        number = false,
+        relativenumber = false,
+	signcolumn = "yes",
+        mappings = {
+          custom_only = false,
+          list = {}
+        }
+      },
+      filters = {
+        dotfiles = false,
+        custom = {}
+      },
+      trash = {
+        cmd = "trash",
+        require_confirm = true,
+      },
+      actions = {
+        change_dir = {
+          enable = true,
+          global = false,
+        },
+        open_file = {
+	  quit_on_open = false,
+	  resize_window = false,
+	  window_picker = {
+	    enable = true,
+	    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+	    exclude = {
+	      filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame", },
+	      buftype  = { "nofile", "terminal", "help", },
+	    }
+	  }
+        },
+      }
+    }
+--]]
