0babf140ec02f85e3753d9d7340d4861eece570f
Petar Metodiev <petar.metodiev@gmail.com>
2022-03-06 22:30:35 +0200

    nvim: do not move to next when * is pressed

--- vim/lua-nvim/nvim/lua/user/configs/nvim-hlslens.lua
+++ vim/lua-nvim/nvim/lua/user/configs/nvim-hlslens.lua
@@ -1,11 +1,16 @@
 require('user/utils')
 
-nnoremap('n', '<Cmd>execute("norm! " . v:count1 . "n")<CR><Cmd>lua require("hlslens").start()<CR>')
-nnoremap('N', '<Cmd>execute("norm! " . v:count1 . "N")<CR><Cmd>lua require("hlslens").start()<CR>')
-nnoremap('*', '*<Cmd>lua require("hlslens").start()<CR>')
-nnoremap('#', '#<Cmd>lua require("hlslens").start()<CR>')
-nnoremap('g*', 'g*<Cmd>lua require("hlslens").start()<CR>')
-nnoremap('g#', 'g#<Cmd>lua require("hlslens").start()<CR>')
+-- nnoremap('n', '<Cmd>execute("norm! " . v:count1 . "n")<CR><Cmd>lua require("hlslens").start()<CR>')
+-- nnoremap('N', '<Cmd>execute("norm! " . v:count1 . "N")<CR><Cmd>lua require("hlslens").start()<CR>')
+-- nnoremap('*', '*<Cmd>lua require("hlslens").start()<CR>')
+-- nnoremap('#', '#<Cmd>lua require("hlslens").start()<CR>')
+-- nnoremap('g*', 'g*<Cmd>lua require("hlslens").start()<CR>')
+-- nnoremap('g#', 'g#<Cmd>lua require("hlslens").start()<CR>')
+
+api.nvim_set_keymap('n', '*', [[<Plug>(asterisk-z*)<Cmd>lua require('hlslens').start()<CR>]], {})
+api.nvim_set_keymap('n', '#', [[<Plug>(asterisk-z#)<Cmd>lua require('hlslens').start()<CR>]], {})
+api.nvim_set_keymap('n', 'g*', [[<Plug>(asterisk-gz*)<Cmd>lua require('hlslens').start()<CR>]], {})
+api.nvim_set_keymap('n', 'g#', [[<Plug>(asterisk-gz#)<Cmd>lua require('hlslens').start()<CR>]], {})
 
 require('hlslens').setup({
     -- remove highlight when the cursor moves outside of highlighted word
