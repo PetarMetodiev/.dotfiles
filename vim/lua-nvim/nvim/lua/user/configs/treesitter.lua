aa6b7d89b309dea130b29b996b36fc2698039b0b
Petar Metodiev <petar.metodiev@gmail.com>
2022-03-08 16:15:54 +0200

    nvim: add more treesitter keymappings

--- vim/lua-nvim/nvim/lua/user/configs/treesitter.lua
+++ vim/lua-nvim/nvim/lua/user/configs/treesitter.lua
@@ -65,7 +65,29 @@ configs.setup({
                 -- You can use the capture groups defined in textobjects.scm
                 ['af'] = '@function.outer',
                 ['if'] = '@function.inner',
-                ['ac'] = '@comment.outer',
+                ['sc'] = '@comment.outer',
+                ['ic'] = '@conditional.inner',
+                ['ac'] = '@conditional.outer',
+            },
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                [']m'] = '@function.outer',
+                [']]'] = '@class.outer',
+            },
+            goto_next_end = {
+                [']M'] = '@function.outer',
+                [']['] = '@class.outer',
+            },
+            goto_previous_start = {
+                ['[m'] = '@function.outer',
+                ['[['] = '@class.outer',
+            },
+            goto_previous_end = {
+                ['[M'] = '@function.outer',
+                ['[]'] = '@class.outer',
             },
         },
     },
