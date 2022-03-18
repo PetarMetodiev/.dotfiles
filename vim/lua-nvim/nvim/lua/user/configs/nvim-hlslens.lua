530821c88014181281226fc1acb4407df6a6932a
Petar Metodiev <petar.metodiev@gmail.com>
2022-03-18 10:14:05 +0200

    nvim: refactor hlslens config

--- vim/lua-nvim/nvim/lua/user/configs/nvim-hlslens.lua
+++ vim/lua-nvim/nvim/lua/user/configs/nvim-hlslens.lua
@@ -7,13 +7,15 @@ if not status_ok then
 end
 
 -- config without asterisk-z integration
-nnoremap('n', '<Cmd>execute("norm! " . v:count1 . "n")<CR><Cmd>lua require("hlslens").start()<CR>')
-nnoremap('N', '<Cmd>execute("norm! " . v:count1 . "N")<CR><Cmd>lua require("hlslens").start()<CR>')
+-- nnoremap('n', '<Cmd>execute("norm! " . v:count1 . "n")<CR><Cmd>lua require("hlslens").start()<CR>')
+-- nnoremap('N', '<Cmd>execute("norm! " . v:count1 . "N")<CR><Cmd>lua require("hlslens").start()<CR>')
 -- nnoremap('*', '*<Cmd>lua require("hlslens").start()<CR>')
 -- nnoremap('#', '#<Cmd>lua require("hlslens").start()<CR>')
 -- nnoremap('g*', 'g*<Cmd>lua require("hlslens").start()<CR>')
 -- nnoremap('g#', 'g#<Cmd>lua require("hlslens").start()<CR>')
 
+nnoremap('n', '<Cmd>execute("norm! " . v:count1 . "n")<CR><Cmd>lua require("hlslens").start()<CR>')
+nnoremap('N', '<Cmd>execute("norm! " . v:count1 . "N")<CR><Cmd>lua require("hlslens").start()<CR>')
 api.nvim_set_keymap('n', '*', [[<Plug>(asterisk-z*)<Cmd>lua require('hlslens').start()<CR>]], {})
 api.nvim_set_keymap('n', '#', [[<Plug>(asterisk-z#)<Cmd>lua require('hlslens').start()<CR>]], {})
 api.nvim_set_keymap('n', 'g*', [[<Plug>(asterisk-gz*)<Cmd>lua require('hlslens').start()<CR>]], {})
