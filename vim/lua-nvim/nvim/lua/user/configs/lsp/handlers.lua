8b7d151067d76bff110d28bd23c5c36283940f99
Petar Metodiev <petar.metodiev@gmail.com>
2022-03-19 14:10:47 +0200

    nvim: use only null-ls for json and html formatting

--- vim/lua-nvim/nvim/lua/user/configs/lsp/handlers.lua
+++ vim/lua-nvim/nvim/lua/user/configs/lsp/handlers.lua
@@ -107,6 +107,16 @@ M.on_attach = function(client, bufnr)
         client.resolved_capabilities.document_range_formatting = false
     end
 
+    if client.name == 'jsonls' then
+        client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
+    end
+
+    if client.name == 'html' then
+        client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
+    end
+
     lsp_keymaps(bufnr)
     lsp_highlight_document(client)
 end
