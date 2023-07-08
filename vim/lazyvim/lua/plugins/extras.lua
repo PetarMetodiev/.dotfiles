86d8717c166bba28eb91d0b0a36499914860fdfa
Petar Metodiev <petar.metodiev@gmail.com>
2023-07-08 23:10:10 +0300

    simplify noice config

--- vim/lazyvim/lua/plugins/extras.lua
+++ vim/lazyvim/lua/plugins/extras.lua
@@ -328,10 +328,6 @@ return {
     event = "VeryLazy",
     opts = {
       presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = true,
         lsp_doc_border = true,
       },
     },
