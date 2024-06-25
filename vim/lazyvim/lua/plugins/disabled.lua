e72a0dc54e280977e1725c0ef88b9331a6e00db6
Petar Metodiev <petar.metodiev@gmail.com>
2024-06-25 19:29:06 +0300

    nvim: enable mini.ai

--- vim/lazyvim/lua/plugins/disabled.lua
+++ vim/lazyvim/lua/plugins/disabled.lua
@@ -2,7 +2,6 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
   { "echasnovski/mini.surround", enabled = false },
-  { "echasnovski/mini.ai", enabled = false },
   { "echasnovski/mini.bufremove", enabled = false },
   { "gbprod/yanky.nvim", enabled = false },
 }
