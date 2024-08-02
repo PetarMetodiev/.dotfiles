f23de4015aa6e1db489867091895e20798fbb0bc
Petar Metodiev <petar.metodiev@gmail.com>
2024-08-02 12:21:29 +0300

    nvim: cleanup disabled plugins
    They are not included by default anymore.

--- vim/lazyvim/lua/plugins/disabled.lua
+++ vim/lazyvim/lua/plugins/disabled.lua
@@ -1,7 +1,4 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
-  { "echasnovski/mini.surround", enabled = false },
-  { "echasnovski/mini.bufremove", enabled = false },
-  { "gbprod/yanky.nvim", enabled = false },
 }
