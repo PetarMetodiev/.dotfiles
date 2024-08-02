dbcf0820d02a7dc9d057560c52170e3439c4eae1
Petar Metodiev <petar.metodiev@gmail.com>
2024-08-02 15:18:28 +0300

    nvim: tune color blind filters

--- vim/lazyvim/lua/plugins/extras.lua
+++ vim/lazyvim/lua/plugins/extras.lua
@@ -13,8 +13,8 @@ return {
           enable = true, -- Enable colorblind support
           simulate_only = false, -- Only show simulated colorblind colors and not diff shifted
           severity = {
-            protan = 0.3, -- Severity [0,1] for protan (red)
-            deutan = 0.8, -- Severity [0,1] for deutan (green)
+            protan = 0.5, -- Severity [0,1] for protan (red)
+            deutan = 0.2, -- Severity [0,1] for deutan (green)
             tritan = 0.5, -- Severity [0,1] for tritan (blue)
           },
         },
