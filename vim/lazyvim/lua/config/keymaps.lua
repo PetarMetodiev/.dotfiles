48125d30203b2a10bc24dedd119772119a73eb11
Petar Metodiev <petar.metodiev@gmail.com>
2025-01-17 13:38:49 +0200

    nvim: keep cursor position on yank

--- vim/lazyvim/lua/config/keymaps.lua
+++ vim/lazyvim/lua/config/keymaps.lua
@@ -110,3 +110,23 @@ end
 
 vim.keymap.del("n", "H")
 vim.keymap.del("n", "L")
+
+-- https://nanotipsforvim.prose.sh/sticky-yank
+-- keep cursor position when yanking
+local cursorPreYank
+vim.keymap.set({ "n", "x" }, "y", function()
+  cursorPreYank = vim.api.nvim_win_get_cursor(0)
+  return "y"
+end, { expr = true })
+vim.keymap.set("n", "Y", function()
+  cursorPreYank = vim.api.nvim_win_get_cursor(0)
+  return "y$"
+end, { expr = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    if vim.v.event.operator == "y" and cursorPreYank then
+      vim.api.nvim_win_set_cursor(0, cursorPreYank)
+    end
+  end,
+})
